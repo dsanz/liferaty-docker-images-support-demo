cd1fd4644e41c8682e43847d3b4face3d1ec8da2
Daniel Sanz <daniel.sanz@liferay.com>
2020-02-06 09:47:19 +0100

    update iteration number

--- multi-container/03_replicated_liferay+mysql+elasticsearch-6/liferay/scripts/wait-for-mysql_and_elasticsearch.sh
+++ multi-container/03_replicated_liferay+mysql+elasticsearch-6/liferay/scripts/wait-for-mysql_and_elasticsearch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 chmod a+x /opt/liferay/wait-for-it.sh
-bash /opt/liferay/wait-for-it.sh -s -t 60 elasticsearch_02:9300
-bash /opt/liferay/wait-for-it.sh -s -t 60 mysql_02:3306
+bash /opt/liferay/wait-for-it.sh -s -t 60 elasticsearch_03:9300
+bash /opt/liferay/wait-for-it.sh -s -t 60 mysql_03:3306
